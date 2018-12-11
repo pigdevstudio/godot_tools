f4d11dc9706ba4cee22a09c1a36bbf89dc808945
Henrique Campos <henriquecampos@pigdev.studio>
2018-12-11 16:42:35 -0200

    Add Batch Rename and In ScriptEditor ColorPick

---
+++ source/plugins/batch_rename/plugin.gd
@@ -0,0 +1,32 @@
+tool
+extends EditorPlugin
+
+func _ready():
+	get_editor_interface().get_selection().connect("selection_changed", self,
+			"_on_selection_changed")
+	set_process_input(false)
+	
+func _on_selection_changed():
+	if get_editor_interface().get_selection().get_selected_nodes().size() > 1:
+		set_process_input(true)
+	else:
+		set_process_input(false)
+
+func _input(event):
+	if Input.is_key_pressed(KEY_F12):
+		var r = load("res://addons/batch_rename/rename.tscn").instance()
+		r.get_node("LineEdit").connect("text_entered", self,
+				"_on_text_entered", [r])
+		add_child(r)
+		
+func _on_text_entered(new_text, interface):
+	var nodes = get_editor_interface().get_selection().get_selected_nodes()
+	nodes.sort_custom(self, "sort_by_index")
+	
+	for n in nodes:
+		n.name = new_text
+	
+	interface.queue_free()
+	
+func sort_by_index(a, b):
+	return a.get_index() < b.get_index()
---
+++ source/plugins/code_color_pick/plugin.gd
@@ -0,0 +1,38 @@
+tool
+extends EditorPlugin
+
+func _ready():
+	get_editor_interface().get_script_editor().connect("editor_script_changed",
+			self, "_on_editor_script_changed")
+			
+	find_text_editor(get_editor_interface().get_script_editor())
+
+func _on_editor_script_changed(new_script):
+	find_text_editor(get_editor_interface().get_script_editor())
+
+func _on_cursor_changed(text_edit):
+	var line = text_edit.get_line(text_edit.cursor_get_line())
+	
+	if line.ends_with("Color"):
+		_show_color(text_edit)
+
+func find_text_editor(node):
+	for c in node.get_children():
+		if c is TextEdit:
+			if c.is_connected("cursor_changed", self, "_on_cursor_changed"):
+				c.disconnect("cursor_changed", self, "_on_cursor_changed")
+			c.connect("cursor_changed", self, "_on_cursor_changed", [c])
+		if c.get_child_count() > 0:
+			find_text_editor(c)
+
+func _show_color(text_edit):
+	if has_node("ColorPicker"):
+		return
+	
+	var picker = load("res://addons/code_color_pick/interface.tscn").instance()
+	add_child(picker)
+	picker.rect_position = get_viewport().get_mouse_position()
+	
+	yield(picker.get_node("Button"), "button_up")
+	text_edit.insert_text_at_cursor("(" + str(picker.color) + ")")
+	picker.queue_free()
