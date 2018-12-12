26e8fad4f568da8e5725e52781ca450c8b891760
Henrique Campos <henriquecampos@pigdev.studio>
2018-12-12 10:30:07 -0200

    Add Inspector Drag n Drop

---
+++ source/plugins/drag_n_drop/plugin.gd
@@ -0,0 +1,63 @@
+tool
+extends EditorPlugin
+
+var current_text_edit = null
+signal dropped
+
+func _ready():
+	get_editor_interface().get_script_editor().connect("editor_script_changed",
+			self, "_on_editor_script_changed")
+			
+	find_text_editor(get_editor_interface().get_script_editor())
+	
+	connect("main_screen_changed", self, "_on_main_screen_changed")
+	
+	set_process_input(false)
+	set_process(false)
+	set_physics_process(false)
+	
+func _on_main_screen_changed(new_screen):
+	set_process_input(new_screen == "Script")
+
+func find_text_editor(node):
+	for c in node.get_children():
+		if c is TextEdit:
+			if c.is_connected("mouse_entered", self, "_on_mouse_entered"):
+				c.disconnect("mouse_entered", self, "_on_mouse_entered")
+			c.connect("mouse_entered", self, "_on_mouse_entered", [c])
+		if c.get_child_count() > 0:
+			find_text_editor(c)
+
+func _on_editor_script_changed(new_script):
+	find_text_editor(get_editor_interface().get_script_editor())
+	
+func _on_mouse_entered(text_edit):
+	current_text_edit = text_edit
+	
+	var data = get_viewport().gui_get_drag_data()
+	if not data:
+		return
+	
+	var i = load("res://addons/drag_n_drop/interface.tscn").instance()
+	text_edit.add_child(i)
+	
+	yield(self, "dropped")
+	i.queue_free()
+
+func _input(event):
+	if current_text_edit == null:
+		return
+	if not event is InputEventMouseButton:
+		return
+	if event.is_pressed():
+		return
+	
+	var data = get_viewport().gui_get_drag_data()
+	if not data:
+		return
+	var path = get_editor_interface().get_edited_scene_root().get_path_to(data.object)
+	var text = "$%s.%s"%[path, data.property]
+	if Input.is_key_pressed(KEY_SHIFT):
+		text = text + " = "
+	current_text_edit.insert_text_at_cursor(text)
+	emit_signal("dropped")
