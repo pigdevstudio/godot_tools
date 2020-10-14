41703cc1c2506cd71b98bda8a1cd4e0f6ded3e29
Henrique Campos <henriquecampos@pigdev.studio>
2020-10-14 15:53:15 -0300

    Update batch_dependency_fixer.gd

--- source/tools/batch_dependency_fixer.gd
+++ source/tools/batch_dependency_fixer.gd
@@ -1,54 +1,51 @@
 tool
 extends EditorScript
 
-#Path to the dependencies that must be checked, leave res:// for 
-#global dependency fix
-const PATH = "res://"
 
 func _run():
 	var interface = get_editor_interface()
 	var filesys = interface.get_resource_filesystem()
-	var dir = filesys.get_filesystem_path(PATH)
+	var dir = filesys.get_filesystem_path(get_editor_interface().get_selected_path())
 	
 	var res = filesys.get_filesystem()
-	
-	#Take all the files in the PATH directory 
+
 	for f in dir.get_file_count():
 		var path = dir.get_file_path(f)
 		var dependencies = ResourceLoader.get_dependencies(path)
 		var file = File.new()
-		
-		# Check if the dependency is broken, if not it will skip it
+
 		for d in dependencies:
 			if file.file_exists(d):
 				continue
 			fix_dependency(d, res, path)
-			
-	#Take all the files in all PATH subdirectories
+
 	for subdir in dir.get_subdir_count():
 		subdir = dir.get_subdir(subdir)
 		for f in subdir.get_file_count():
 			var path = subdir.get_file_path(f)
 			var dependencies = ResourceLoader.get_dependencies(path)
+			if dependencies.size() < 1:
+				continue
 			var file = File.new()
 			for d in dependencies:
 				if file.file_exists(d):
 					continue
 				fix_dependency(d, res, path)
+	get_editor_interface().get_resource_filesystem().scan()
+
 
 func fix_dependency(dependency, directory, resource_path):
-	#Recursively call to enter in all the subdirectories until it reaches
-	#the last subdir
 	for subdir in directory.get_subdir_count():
 		fix_dependency(dependency, directory.get_subdir(subdir), resource_path)
-		
-	#Fix all the files it finds during the recursive call
+
 	for f in directory.get_file_count():
 		if not directory.get_file(f) == dependency.get_file():
 			continue
 		var file = File.new()
-		file.open(resource_path, file.READ_WRITE)
+		file.open(resource_path, file.READ)
 		var text = file.get_as_text()
+		file.close()
 		text = text.replace(dependency, directory.get_file_path(f))
+		file.open(resource_path, file.WRITE)
 		file.store_string(text)
 		file.close()
