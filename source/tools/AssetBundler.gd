cbbc274efded97fafda20e71d28f34df74e570f7
Henrique Campos <henriiquecampos@gmail.com>
2023-07-25 11:36:15 -0300

    Update AssetBundler.gd to Godot 4

--- source/tools/AssetBundler.gd
+++ source/tools/AssetBundler.gd
@@ -1,38 +1,29 @@
-tool
+@tool
 extends EditorScript
 
 
-func _run() -> void:
-	var interface: = get_editor_interface()
-	var current_path: = interface.get_current_path()
-	var directory: = Directory.new()
-	var selected_path: = get_editor_interface().get_selected_path()
-	if directory.open(selected_path) == OK:
-		if directory.make_dir("Bundle") == OK:
-			bundle(current_path, selected_path + "/Bundle/")
-			get_editor_interface().get_resource_filesystem().scan()
-		else:
-			print("An error occurred when trying to create the bundle folder.")
-	else:
-		print("An error occurred when trying to access the path.")
+func _run():
+	var interface = get_editor_interface()
+	var selected_paths = get_editor_interface().get_selected_paths()
+	for path in selected_paths:
+		var directory = DirAccess.open(path.get_base_dir())
+		var file_name = path.get_file().replace("." + path.get_extension(), "")
+		directory.make_dir(file_name)
+		var target_dir = path.get_base_dir() + "/%s/" % file_name
+		directory.copy(path, target_dir + path.get_file())
+		bundle(path, target_dir)
+	get_editor_interface().get_resource_filesystem().scan()
 
 
-func bundle(current_path: String, target_path: String) -> void:
-	var file: = File.new()
-	if file.open(current_path, File.READ_WRITE) == OK:
-		var file_string: = file.get_as_text()
-		file.close()
-		if file.open(target_path + current_path.get_file(), File.WRITE) == OK:
-			var directory: = Directory.new()
-			for dependency in ResourceLoader.get_dependencies(current_path):
-				var new_dependency_path: String = target_path + dependency.get_file()
-				if directory.copy(dependency, new_dependency_path) == OK:
-					file_string.replace(dependency, new_dependency_path)
-					if ResourceLoader.get_dependencies(dependency).size() > 0:
-						bundle(dependency, target_path)
-			file.store_string(file_string)
-			file.close()
-		else:
-			print("An error occurred when trying to open the file.")
-	else:
-		print("An error occurred when trying to open the file.")
+func bundle(selected_file, target_path):
+	for dependency in ResourceLoader.get_dependencies(selected_file):
+		var erasing_start = dependency.find("res://")
+		var erased = dependency.erase(erasing_start, dependency.length())
+		var dependency_path = dependency.replace(erased, "")
+		var new_dependency_path = target_path + dependency_path.get_file()
+		var directory = DirAccess.open(target_path.get_base_dir())
+		directory.copy(dependency_path, new_dependency_path)
+		for sub_dependency in ResourceLoader.get_dependencies(dependency_path):
+			bundle(dependency_path, target_path)
+	var directory = DirAccess.open(target_path)
+	directory.copy(selected_file, target_path)
